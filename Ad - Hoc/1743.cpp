54f9a3431a806bac8a9ed2a5758c08adb0f0a7b5
Estela Miranda Batista <estelambatista@gmail.com>
2021-07-09 15:49:10 -0300

    📌 Exercícios Maratona Interna BOTz, Semana 1 | 2021/2

---
+++ Ad - Hoc/1743.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a[5],b[5];
+    for(int i=0;i<5;i++) cin>>a[i];
+    for(int i=0;i<5;i++) cin>>b[i];
+    
+    int i=0;
+    bool x=true;
+
+    while(i<5){
+        if(a[i]==b[i]){
+            x=false;
+            break;
+        }
+        i++;
+    }
+
+    if(x) cout<<"Y"<<endl;
+    else cout<<"N"<<endl;
+}
---
+++ Iniciante/2759.py
@@ -0,0 +1,7 @@
+x = input("")
+y = input("")
+z = input("")
+
+print("A = "+x+", B = "+y+", C = "+z)
+print("A = "+y+", B = "+z+", C = "+x)
+print("A = "+z+", B = "+x+", C = "+y)
